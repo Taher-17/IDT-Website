e7342a9e34e4eff165601248b7d0741def340df5
Taher Ismail <taher.ismail17@icloud.com>
2025-10-14 21:25:09 +1300

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://innovativedigitaltechnologies.software", // your preferred domain
+  generateRobotsTxt: true, // automatically generates robots.txt
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+};
